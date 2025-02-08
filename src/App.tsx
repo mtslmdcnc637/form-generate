99a239302f87a7cf5e09c853a892a5b1fdc7a691
mateus-lima <76916441+mtslmdcnc637@users.noreply.github.com>
2025-02-08 09:50:21 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FormGenerator from './components/FormGenerator';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <FormGenerator />
+    </div>
+  );
+}
+
+export default App;
